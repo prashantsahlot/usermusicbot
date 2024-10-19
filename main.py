7164d5eb259737ceaaaaf7417eaf314c9b38cc1f
Frozen owner <156554454+prashantsahlot@users.noreply.github.com>
2024-10-19 14:19:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -182,7 +182,6 @@ async def revert(client: Client, message: Message):
     except Exception as e:
         await message.edit(f"`Error reverting: {str(e)}`")
 
-
 @app.on_message(filters.command(["sayang", "lover"], ".") & filters.me)
 async def zeyenk(client: Client, message: Message):
     e = await message.reply("I LOVEE YOUUU 💕")
@@ -211,6 +210,81 @@ async def zeyenk(client: Client, message: Message):
     await e.edit("LOVE YOU 💞")
     # New messages to be added
     await e.edit("💖 You're my star 💖")
+
+# Delete command
+@app.on_message(filters.command("del", ".") & filters.me)
+async def del_msg(client: Client, message: Message):
+    msg_src = message.reply_to_message
+    if msg_src:
+        if msg_src.from_user.id:
+            try:
+                await client.delete_messages(message.chat.id, msg_src.id)
+                await message.delete()
+            except BaseException:
+                pass
+    else:
+        await message.delete()
+
+# Purge command
+@app.on_message(filters.command("purge", ".") & filters.me)
+async def purge(client: Client, message: Message):
+    ex = await message.edit_text("`Starting To Purge Messages!`")
+    msg = message.reply_to_message
+    if msg:
+        itermsg = list(range(msg.id, message.id))
+    else:
+        await ex.edit("`Reply To Message To Purge!`")
+        return
+    count = 0
+
+    for i in itermsg:
+        try:
+            count += 1
+            await client.delete_messages(
+                chat_id=message.chat.id, message_ids=i, revoke=True
+            )
+        except FloodWait as e:
+            await asyncio.sleep(e.x)
+        except Exception as e:
+            await ex.edit(f"**ERROR:** `{e}`")
+            return
+
+    done = await ex.edit(
+        f"**Fast Purge Completed!**\n**Successfully Delete** `{str(count)}` **Message.**"
+    )
+    await asyncio.sleep(2)
+    await done.delete()
+
+# Purge me command
+@app.on_message(filters.command("purgeme", ".") & filters.me)
+async def purgeme(client: Client, message: Message):
+    if len(message.command) != 2:
+        return await message.delete()
+    n = message.text.split(None, 1)[1].strip()
+    if not n.isnumeric():
+        return await message.edit_text("Please enter a number")
+    n = int(n)
+    if n < 1:
+        return await message.edit_text("Enter the number of messages you want to delete!")
+    chat_id = message.chat.id
+    message_ids = [
+        m.id
+        async for m in client.search_messages(
+            chat_id,
+            from_user="me",
+            limit=n,
+        )
+    ]
+    if not message_ids:
+        return await message.edit_text("Could not find message.")
+    to_delete = [message_ids[i: i + 99] for i in range(0, len(message_ids), 99)]
+    for hundred_messages_or_less in to_delete:
+        await client.delete_messages(
+            chat_id=chat_id,
+            message_ids=hundred_messages_or_less,
+            revoke=True,
+        )
+    await message.delete()
 
 # Start the bot and Flask app
 if __name__ == "__main__":
